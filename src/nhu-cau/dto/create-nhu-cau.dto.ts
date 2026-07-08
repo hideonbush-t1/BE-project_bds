0e25588d062b4b193727c8a959c36d62d37cd517
ThaiSon1708 <son0987801073@gmail.com>
2026-07-08 11:23:59 +0700

    feat: cap nhat code giao dich va nhu cau cho BE

--- src/nhu-cau/dto/create-nhu-cau.dto.ts
+++ src/nhu-cau/dto/create-nhu-cau.dto.ts
@@ -2,32 +2,35 @@ import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateNhuCauDto {
   @IsString()
-  khachHangId!: string;
-
-  @IsString()
-  loaiNhuCau!: string;
-
-  @IsString()
-  loaiBDS!: string;
-
-  @IsString()
-  viTri!: string;
+  khachHangId!: string; // Add the '!' here
 
   @IsOptional()
-  @IsNumber()
+  @IsString()
+  nhanVienId?: string;
+
+  @IsOptional()
+  @IsString()
+  loaiNhuCau?: string;
+
+  @IsOptional()
+  @IsString()
+  loaiBDS?: string;
+
+  @IsOptional()
+  @IsString()
+  viTri?: string;
+
+  @IsOptional()
   dienTichMin?: number;
 
   @IsOptional()
-  @IsNumber()
   dienTichMax?: number;
-
-  @IsOptional()
-  giaMin?: string;
-
-  @IsOptional()
-  giaMax?: string;
 
   @IsOptional()
   @IsString()
   ghiChu?: string;
+
+  @IsOptional()
+  @IsString()
+  tinhTrang?: string; 
 }
